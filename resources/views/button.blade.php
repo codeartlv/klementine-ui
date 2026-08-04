f0a34852ee5e8d4762c6b541f3b9a47e4cdc59be
Deniss Kozlovs <deniss@codeart.lv>
2026-08-04 09:55:36 +0300

    Add icon to button

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -1,5 +1,17 @@
-<div class="form-element">
-	<wa-button type="{{ $type }}" {{ $attributes->merge(['class' => $span ? 'block' : '']) }}>
-		{{ $label }}
-	</wa-button>
-</div>
+@props([
+	'icon' => null,
+	'icon_end' => null,
+])
+
+<wa-button type="{{ $type }}" {{ $attributes->merge(['class' => $span ? 'block' : '']) }}>
+	@if($icon)
+		<wa-icon slot="start" name="{{ $icon }}"></wa-icon>
+	@endif
+
+	{{ $label }}
+
+	@if($icon_end)
+		<wa-icon slot="end" name="{{ $icon_end }}"></wa-icon>
+	@endif
+</wa-button>
+
