cf09f006b9d20c930f53873e71935f0fc3037b2f
Deniss Kozlovs <deniss@codeart.lv>
2026-08-04 21:27:31 +0300

    Add margin only when label applied

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -1,26 +1,32 @@
-<wa-select {{$attributes}}>
-	@if($startIcon)
-		<span slot="start">
-			<x-ui-icon name="{{$startIcon}}" />
-		</span>
-	@endif
-	@if($endIcon)
-		<span slot="end">
-			<x-ui-icon name="{{$endIcon}}" />
-		</span>
-	@endif
-	@foreach ($options as $item)
-		@if (method_exists($item, 'options'))
-			<small>{{$item->label}}</small>
-			@foreach ($item->options() as $option)
-				<wa-option value="{{$option->value}}" {{$option->disabled ? 'disabled':''}} {{$option->selected?'selected':''}}>{{$option->label}}</wa-option>
-			@endforeach
+@props([
+	'label' => '',
+])
 
-			@if(!$loop->last)
-				<wa-divider></wa-divider>
-			@endif
-		@else
-			<wa-option value="{{$item->value}}" {{$item->disabled ? 'disabled':''}} {{$item->selected?'selected':''}}>{{$item->label}}</wa-option>
+<div class="form-element">
+	<wa-select label="{{ $label }}" {{$attributes->merge(['class' => $label ? 'has-label' : ''])}}>
+		@if($startIcon)
+			<span slot="start">
+				<x-ui-icon name="{{$startIcon}}" />
+			</span>
 		@endif
-	@endforeach
-</wa-select>
+		@if($endIcon)
+			<span slot="end">
+				<x-ui-icon name="{{$endIcon}}" />
+			</span>
+		@endif
+		@foreach ($options as $item)
+			@if (method_exists($item, 'options'))
+				<small>{{$item->label}}</small>
+				@foreach ($item->options() as $option)
+					<wa-option value="{{$option->value}}" {{$option->disabled ? 'disabled':''}} {{$option->selected?'selected':''}}>{{$option->label}}</wa-option>
+				@endforeach
+
+				@if(!$loop->last)
+					<wa-divider></wa-divider>
+				@endif
+			@else
+				<wa-option value="{{$item->value}}" {{$item->disabled ? 'disabled':''}} {{$item->selected?'selected':''}}>{{$item->label}}</wa-option>
+			@endif
+		@endforeach
+	</wa-select>
+</div>
