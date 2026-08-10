f080e297f72119046cb7d5e23320363dfc7828a8
Deniss Kozlovs <deniss@codeart.lv>
2026-08-10 17:12:02 +0300

    Change to WA native input

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,9 +1,32 @@
+@props([
+	'icon' => null,
+	'icon_end' => null,
+])
+
+@php
+	$classes = [];
+
+	if ($icon_end) {
+		$classes[] = 'has-icon';
+		$classes[] = 'has-end-icon';
+	}
+@endphp
+
 <div class="form-element">
 	@if ($label)
 		<x-ui-label :text="$label" :required="$required" />
 	@endif
 
-	<input class="form-input" name="{{ $name }}" {{ $attributes }} value="{{ $value }}" />
+	<wa-input {{ $attributes }} name="{{ $name }}" value="{{ $value }}">
+		@if($icon)
+			<wa-icon slot="start" name="{{ $icon }}"></wa-icon>
+		@endif
+
+		@if($icon_end)
+			<wa-icon slot="end" name="{{ $icon_end }}"></wa-icon>
+		@endif
+	</wa-input>
+
 
 	@if ($hint)
 		<div class="form-hint">{{ $hint }}</div>
